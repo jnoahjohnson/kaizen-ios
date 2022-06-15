a255dd2d48c4ac1f53f33a4ee9887eb82b049c58
Noah Johnson <jnoahjohnson@gmail.com>
2022-06-14 19:49:05 -0600

    Initial Commit

---
+++ Kaizen/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Kaizen
+//
+//  Created by Noah Johnson on 6/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kaizen/KaizenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaizenApp.swift
+//  Kaizen
+//
+//  Created by Noah Johnson on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct KaizenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
